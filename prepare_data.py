e8a0e5e79a916c388d0db2ff50e97d259b4d4b6d
Gealor <gzazulya@gmail.com>
2025-09-16 12:10:15 +0400

    add readme.md

--- prepare_data.py
+++ prepare_data.py
@@ -35,6 +35,7 @@ EMOTIONS_TO_NUM = {
 def exctract_features(file_path: pathlib.Path, max_pad_len=200):
     audio, sr = librosa.load(file_path, sr=22050)
     mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=40)
+    # нормализация длины (обрезаю/дополняю нулями)
     if mfcc.shape[1]<max_pad_len:
         pad_width = max_pad_len - mfcc.shape[1]
         mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
