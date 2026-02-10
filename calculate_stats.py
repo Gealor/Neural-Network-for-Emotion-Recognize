878ba8172c0330bf08d6c34e4b0ba126acc3e4c4
Gealor <gzazulya@gmail.com>
2026-02-10 19:17:18 +0400

    calculate_stats refactor, added function

--- calculate_stats.py
+++ calculate_stats.py
@@ -1,15 +1,15 @@
 import numpy as np
 
+def calculate():
+    print("Расчет mean и std для нормализации...")
+    X_train = np.load('processed_data/X_train.npy', mmap_mode='r')
 
-print("Расчет mean и std для нормализации...")
-X_train = np.load('processed_data/X_train.npy', mmap_mode='r')
+    # Расчет mean и std по оси частот (axis=1 в 2D представлении)
+    mean = np.mean(X_train, axis=(0, 2), keepdims=True)
+    std = np.std(X_train, axis=(0, 2), keepdims=True)
 
-# Расчет mean и std по оси частот (axis=1 в 2D представлении)
-mean = np.mean(X_train, axis=(0, 2), keepdims=True)
-std = np.std(X_train, axis=(0, 2), keepdims=True)
+    np.save('processed_data/mean.npy', mean)
+    np.save('processed_data/std.npy', std)
 
-np.save('processed_data/mean.npy', mean)
-np.save('processed_data/std.npy', std)
-
-print("Mean и std сохранены.")
-print("Форма mean:", mean.shape)
+    print("Mean и std сохранены.")
+    print("Форма mean:", mean.shape)
