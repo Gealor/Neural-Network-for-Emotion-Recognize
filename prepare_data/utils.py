d91675b720e34ecab82c40e48cef228e35e2d2ac
Gealor <gzazulya@gmail.com>
2026-02-17 19:18:29 +0400

    update extract_features function

--- prepare_data/utils.py
+++ prepare_data/utils.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 import librosa
+from matplotlib import pyplot as plt
 import numpy as np
 
 import config
@@ -40,12 +42,36 @@ def time_shift(audio, shift_max_ratio=0.2):
 
 
 def extract_features(audio, sr, n_mels=config.HEIGHT, max_pad_len=config.WIDTH):
+    # удаление тишины
+    audio, _ = librosa.effects.trim(audio, top_db=25)
+
     mel_spectrogram = librosa.feature.melspectrogram(y=audio, sr=sr, n_fft=2048, hop_length=512, n_mels=n_mels)
     log_mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)
+
+    log_mel_spectrogram = np.maximum(log_mel_spectrogram, -80.0)
+
+    current_len = log_mel_spectrogram.shape[1]
     # нормализация длины (обрезаю/дополняю нулями)
-    if log_mel_spectrogram.shape[1] < max_pad_len:
-        pad_width = max_pad_len - log_mel_spectrogram.shape[1]
-        log_mel_spectrogram = np.pad(log_mel_spectrogram, pad_width=((0, 0), (0, pad_width)), mode='constant')
+    if current_len < max_pad_len:
+        pad_total = max_pad_len - current_len
+        pad_left = pad_total // 2
+        pad_right = pad_total - pad_left
+        log_mel_spectrogram = np.pad(
+            log_mel_spectrogram,
+            pad_width=((0, 0), (pad_left, pad_right)),
+            mode='constant',
+            constant_values=-80
+        )
     else:
-        log_mel_spectrogram = log_mel_spectrogram[:, :max_pad_len]
-    return log_mel_spectrogram
+        start = (current_len - max_pad_len) // 2
+        log_mel_spectrogram = log_mel_spectrogram[:, start : start + max_pad_len]
+    
+    # return log_mel_spectrogram
+
+    # Вычисляем первую и вторую производные
+    delta = librosa.feature.delta(log_mel_spectrogram)
+    delta2 = librosa.feature.delta(log_mel_spectrogram, order=2)
+    
+    # Склеиваем в 3 канала: (128, 128, 3)
+    features = np.stack([log_mel_spectrogram, delta, delta2], axis=-1)
+    return features
